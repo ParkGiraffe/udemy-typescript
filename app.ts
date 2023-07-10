0c0cf8198686ef2499be24019a3afe580a04c70c
ParkGiraffe <op5321@soongsil.ac.kr>
2023-07-10 16:05:32 +0900

    Study: 2-18 done

--- app.ts
+++ app.ts
@@ -1,21 +1,37 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-  //   throw new Error('Incorrect input!');
-  // }
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
+const person = {
+  name: "Giraffe",
+  age: 22,
+  hobbies: ["Sports", "Cooking"],
+};
+
+let favoriteActivites: string[];
+favoriteActivites = ["Sports"];
+
+for (const hobby of favoriteActivites) {
+  console.log(hobby.toUpperCase);
 }
 
-let number1: number;
-number1 = 5;
-// number1 = '5'; // 한 번 지정된 타입은 바꿀 수 없다.
+console.log(person.name);
 
-const number2 = 2.8; // 굳이 타입형을 명시하지 않아도, 타입스크립트 알아서 옆에 쓰인 value 값을 보고 타입 추론을 한다.
-const printResult = true;
-let resultPhrase = 'Result is: ';
+// 중첩된 개체 및 타입
+const product = {
+  id: "abc1",
+  price: 12.99,
+  tags: ["great-offer", "hot-and-new"],
+  details: {
+    title: "Red Carpet",
+    description: "A great carpet - almost brand-new!",
+  },
+};
 
-add(number1, number2, printResult, resultPhrase);
+/*
+{
+  id: string;
+  price: number;
+  tags: string[];
+  details: {
+    title: string;
+    description: string;
+  }
+}
+*/
