784d6c06e5ca501a72b2473facbd8b1dd87faca0
ParkGiraffe <op5321@soongsil.ac.kr>
2023-08-08 18:32:55 +0900

    Study : 2-19 done

--- app.ts
+++ app.ts
@@ -2,11 +2,24 @@ const person: {
   name: string;
   age: number;
   hobbies: string[];
+  role : [number, string];
+  // role : readonly [number, string];
 } = {
   name: "Giraffe",
   age: 22,
   hobbies: ["Sports", "Cooking"],
+  role: [1, 'blogger'],
 };
+
+
+// person.role[1] = '1'
+// person.role[1] = 1
+
+// person.role = [1, '1'];
+// person.role = [1, 1];
+// person.role = [1, '1', 1];
+
+// person.role.push(1)
 
 console.log(person.name);
 // console.log(person.nickname);
