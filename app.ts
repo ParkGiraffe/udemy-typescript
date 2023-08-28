cfabf3234aae6c711334c2906d00ef51b98a27d3
ParkGiraffe <op5321@soongsil.ac.kr>
2023-08-28 16:16:56 +0900

    Study : 2-23 done

--- app.ts
+++ app.ts
@@ -1,12 +1,21 @@
 let uni: number | string | boolean;
-uni = 1;
-uni = "1";
-uni = false;
+let lit: "as" | "k" | 5 | true;
 
-function add(n1: number | string, n2: number | string) {
+lit = 'as';
+lit = 4
+
+function add(
+  n1: number | string,
+  n2: number | string,
+  resultType: "as-number" | "as-string"
+) {
   let result: any;
 
-  if (typeof n1 === "number" && typeof n2 === "number") {
+  if (
+    typeof n1 === "number" &&
+    typeof n2 === "number" &&
+    resultType === "as-number"
+  ) {
     result = n1 + n2;
   } else {
     result = n1.toString() + n2.toString();
@@ -14,3 +23,6 @@ function add(n1: number | string, n2: number | string) {
 
   return result;
 }
+
+
+add(0, 1, 'as-type')
