1569dcf068d76dbef40d808f52946914cfb8b3b6
ParkGiraffe <op5321@soongsil.ac.kr>
2023-09-01 16:38:56 +0900

    Study : 2-28 done

--- app.ts
+++ app.ts
@@ -29,3 +29,12 @@ function printResult(num: number): void {
   console.log("Result: " + num);
   // return;
 }
+
+function addAndHandle(n1: number, n2: number, callback: (num: number) => void) {
+  const result = n1 + n2;
+  callback(result);
+}
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
