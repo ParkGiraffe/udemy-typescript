74939df65cd778b1f91fec9e5d6b2e1306299fd4
ParkGiraffe <op5321@soongsil.ac.kr>
2023-10-27 16:35:27 +0900

    Study : 6-84 done

--- section6/src/app.ts
+++ section6/src/app.ts
@@ -17,8 +17,54 @@ const e1: ElevatedEmployee = {
   startDate: new Date(),
 };
 
-
-type Combinable = string | number | boolean;
-type Numeric = number | boolean;
+type Combinable = string | number;
+type Numeric = number;
 type Universal = Combinable & Numeric;
 // 유니온 타입끼리 교차시켜서, 교집함인 타입을 찾아낸다.
+
+const add = (a: Combinable, b: Combinable) => {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+};
+
+type UnknownEmployee = Employee | Admin;
+
+const printEmployeeInformation = (emp: UnknownEmployee) => {
+  console.log("Name: " + emp.name);
+
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+
+  if ("startDate" in emp) {
+    console.log("Start Date: " + emp.startDate);
+  }
+};
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck");
+  }
+
+  loadCargo(amount: number) {
+    console.log("Loading cargo ..." + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const useVehicle = (vehicle: Vehicle) => {
+  vehicle.drive();
+
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+};
