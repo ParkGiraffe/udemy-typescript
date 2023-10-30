ddf9696fd7c2eb1d5fdc18b4b1142e2cbe230994
ParkGiraffe <op5321@soongsil.ac.kr>
2023-10-30 16:30:18 +0900

    Study : 6-85 done

--- section6/src/app.ts
+++ section6/src/app.ts
@@ -68,3 +68,30 @@ const useVehicle = (vehicle: Vehicle) => {
     vehicle.loadCargo(1000);
   }
 };
+
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+const moveAnimal = (animal: Animal) => {
+  let speed;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+  }
+
+  console.log("Moving at speed: " + speed);
+};
+
+moveAnimal({ type: "bird", flyingSpeed: 10 });
