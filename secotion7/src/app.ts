7d1b1f8f9679cc9af38a8ab2fe9d9ac50aa75526
ParkGiraffe <op5321@soongsil.ac.kr>
2023-11-09 16:10:17 +0900

    Study : 7-95 done

--- secotion7/src/app.ts
+++ secotion7/src/app.ts
@@ -1,1 +1,15 @@
-// Code goes here!
+// const names: Array = [];
+
+// function merge(objA: object, objB: object) {
+//   return Object.assign(objA, objB);
+// }
+
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "Max", hobbies: ['game'] }, { age: 30 });
+
+mergedObj.age;
+mergedObj.hobbies;
