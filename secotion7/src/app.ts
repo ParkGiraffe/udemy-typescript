44e94d53f2b089dbf9bb53e99bd55ff5a72e4a5f
ParkGiraffe <op5321@soongsil.ac.kr>
2023-11-13 16:35:10 +0900

    Study : 7-96 done

--- secotion7/src/app.ts
+++ secotion7/src/app.ts
@@ -4,12 +4,37 @@
 //   return Object.assign(objA, objB);
 // }
 
-
 function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
-const mergedObj = merge({ name: "Max", hobbies: ['game'] }, { age: 30 });
+const mergedObj = merge({ name: "Max", hobbies: ["game"] }, { age: 30 });
 
 mergedObj.age;
 mergedObj.hobbies;
+
+
+interface Lengthy {
+  length: number;
+}
+
+
+function countAndDescription<T extends Lengthy>(element: T) {
+  let descriptionText = "Got no value";
+  if (element.length === 1) {
+    descriptionText = "Got 1 element.";
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements.";
+  }
+
+  return [element, descriptionText];
+}
+
+
+countAndDescription('hello');
+countAndDescription([1, 2]);
+
+// countAndDescription({a: 'a'})
+
+
+countAndDescription({a: 'a', length: 1})
