7f42c9588ecce89f8d1caebe18d9e5698b5e3c57
ParkGiraffe <op5321@soongsil.ac.kr>
2023-11-27 16:04:35 +0900

    Study : 8-109 done

--- section8/src/app.ts
+++ section8/src/app.ts
@@ -19,6 +19,34 @@ function WithTemplate(template: string, hookId: string) {
   };
 }
 
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator!");
+  console.log(target, propertyName);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid price - should be positive!");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this.price * (1 + tax);
+  }
+}
+
 @Logger("LOGGING - PERSON")
 @WithTemplate("<h1>Person Object</h1>", "app")
 class Person {
