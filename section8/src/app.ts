69a202f65ffdca51c536876009aad83bbf764d9d
ParkGiraffe <op5321@soongsil.ac.kr>
2023-12-05 11:08:09 +0900

    Study : 8-114 done

--- section8/src/app.ts
+++ section8/src/app.ts
@@ -76,3 +76,34 @@ class Pet {
     console.log("Createing pet object");
   }
 }
+
+function Autobind(
+  _target: any,
+  _methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+class Printer {
+  message = "This works!";
+
+  @Autobind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+
+const button = document.querySelector("button")!;
+button.addEventListener("click", p.showMessage);
