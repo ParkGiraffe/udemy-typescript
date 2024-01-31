bf7ff35d4504e1f0d4a332a6a7fc2ee65b659cbd
ParkGiraffe <op5321@soongsil.ac.kr>
2024-01-31 12:28:19 +0900

    Study : 12-164 done

--- section12/webpack.config.js
+++ section12/webpack.config.js
@@ -1,31 +1,23 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/app.ts",
-  devServer: {
-    static: [
-      {
-        directory: path.join(__dirname),
-      },
-    ],
-  },
+  mode: 'development',
+  entry: './src/app.ts',
   output: {
-    filename: "app.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/dist/",
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist'
   },
-  devtool: "inline-source-map",
   module: {
     rules: [
       {
-        test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
   },
   resolve: {
-    extensions: [".ts", ".js"],
-  },
+    extensions: ['.ts', '.js']
+  }
 };
