ff9564f14abcf5e1f8cf734072b0901cbb928502
ParkGiraffe <op5321@soongsil.ac.kr>
2024-02-07 12:34:32 +0900

    Study : 14-181 done

--- section14/src/App.tsx
+++ section14/src/App.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import TodoList from "./Components/TodoList";
+import NewTodo from "./Components/NewTodo";
 
 function App() {
   const todos = [{ id: "t1", text: "Finish the course" }];
+
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  };
+
   return (
     <div className="App">
+      <NewTodo onAddTodo={todoAddHandler} />
       <TodoList items={todos} />
     </div>
   );
