2900e923213b6f7a43c212c580f5be7aa4b813cf
ParkGiraffe <op5321@soongsil.ac.kr>
2024-02-08 14:40:24 +0900

    Study : 14-184 done

--- section14/src/Components/TodoList.tsx
+++ section14/src/Components/TodoList.tsx
@@ -2,13 +2,21 @@ import React from "react";
 
 interface TodoListProps {
   items: { id: string; text: string }[];
+  onDeleteTodo: (id: string) => void;
 }
 
 const TodoList: React.FC<TodoListProps> = (props) => {
   return (
     <ul>
       {props.items.map((todo) => {
-        return <li key={todo.id}>{todo.text}</li>;
+        return (
+          <li key={todo.id}>
+            <span>{todo.text}</span>
+            <button onClick={props.onDeleteTodo.bind(null, todo.id)}>
+              DELETE
+            </button>
+          </li>
+        );
       })}
     </ul>
   );
