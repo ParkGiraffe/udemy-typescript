512cda4a49bc976487a67bfaa2144bbb5dc85d8d
ParkGiraffe <op5321@soongsil.ac.kr>
2024-02-08 14:28:36 +0900

    Study : 14-182 done

--- section14/src/App.tsx
+++ section14/src/App.tsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoList from "./Components/TodoList";
 import NewTodo from "./Components/NewTodo";
+import { Todo } from "./todo.model";
 
 function App() {
-  const todos = [{ id: "t1", text: "Finish the course" }];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
-    console.log(text);
+    setTodos([{ id: Math.random().toString(), text: text }]);
   };
 
   return (
